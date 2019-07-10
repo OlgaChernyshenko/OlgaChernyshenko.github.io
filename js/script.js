c7991175e471531f894867b49b4daa67a39168e5
OlgaChernyshenko <olga_super777@ukr.net>
2019-07-10 23:45:05 +0300

    portfolio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-  percentPosition: true
-});
+// // init Masonry
+// var $grid = $('.grid').masonry({
+//   // options...
+// });
+// // layout Masonry after each image loads
+// $grid.imagesLoaded().progress( function() {
+//   $grid.masonry('layout');
+// });
